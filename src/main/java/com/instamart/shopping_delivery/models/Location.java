34599daa2806706daf1d0bd0871ae9ccfa318ab4
Mohd Ahmed <ahmadilahi70@gmail.com>
2025-07-14 02:31:52 +0530

    first commit

---
+++ src/main/java/com/instamart/shopping_delivery/models/Location.java
@@ -0,0 +1,24 @@
+package com.instamart.shopping_delivery.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "Location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+    String address;
+    String country;
+    String state;
+    String city;
+    int pincode;
+    @OneToMany
+    List<Location> locations;
+
+}
---
+++ src/main/java/com/instamart/shopping_delivery/models/WareHouseItem.java
@@ -0,0 +1,20 @@
+package com.instamart.shopping_delivery.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "warehouseItems")
+public class WareHouseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+    UUID wid;
+    UUID pid;
+    int quantity;
+    int discount;
+
+}
